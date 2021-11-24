d49140ee2207eef2aea4b531a23688b7690b7077
nichmorgan-loft <nich.morgan@loft.com.br>
2021-11-24 15:43:16 -0300

    handler validate argments parsing event

--- aws_lambda_utils/interfaces/ihandler.py
+++ aws_lambda_utils/interfaces/ihandler.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Any, TypeVar
 from ..events import SQSEvent, SNSEvent, S3ObjectLambdaEvent, SimpleEmailEvent
 from .icontext import IContext
+from pydantic import validate_arguments
 
 Event = TypeVar(
     "Event",
@@ -14,6 +15,7 @@ Event = TypeVar(
 
 class IHandler(ABC):
     @staticmethod
+    @validate_arguments
     @abstractmethod
     def handler(event: Event, context: IContext) -> Any:
         ...
